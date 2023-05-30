58a4463cb69848eede5343caf73af39aad55901d
decqyd <68643853+decqyd@users.noreply.github.com>
2023-05-30 18:33:07 +0100

    improved errors

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,30 @@ use std::{env, fs};
 mod error;
 mod scanner;
 use error::KlangError;
+use std::path::Path;
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         eprintln!("Error: expected file path");
         std::process::exit(1);
     }
     let filename = &args[1];
+    let path = Path::new(&filename);
+    let relfilename = path.file_name().unwrap().to_str().unwrap();
     if let Err(e) = fs::metadata(filename) {
         eprintln!("Error: {} is not a file: {}", filename, e);
         std::process::exit(1);
     } else {
-        run_file(filename);
+        run_file(filename, relfilename);
     }
+
+    Ok(())
 }
 
-fn run_file(path: &str) {
+fn run_file(path: &str, relfilename: &str) {
     let source = fs::read_to_string(path).expect("failed to read file");
-    let mut scanner = scanner::Scanner::new(&source, path);
+    let mut scanner = scanner::Scanner::new(&source, relfilename);
     scanner.scan_tokens();
     println!("{:?}", scanner.tokens);
     println!("{}", source);
