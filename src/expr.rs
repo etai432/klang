eba804eaa223eaf458113a74a5be537dff09ae84
etai432 <etaizilberman@gmail.com>
2023-05-31 00:51:26 +0300

    ok

--- src/expr.rs
+++ src/expr.rs
@@ -2,13 +2,22 @@ use crate::scanner::{Token, Value};
 
 #[derive(Clone, Debug)]
 pub enum Expr {
-    Assign { name: Token, value: Box<Expr> }, //assignment
-    Binary(Binary),                           //binary operations (+, -, .., ==, !=, <=, ..)
-    Call(Call),
+    Assign {
+        name: Token,
+        value: Box<Expr>,
+    }, //assignment
+    Binary(Binary), //binary operations (+, -, .., ==, !=, <=, ..)
+    Call {
+        callee: Box<Expr>,
+        paren: Token,
+        arguments: Vec<Box<Expr>>,
+    },
     Grouping(Grouping), // "(" expression ")"
     Literal(Value),
     Logical(Logical), // and / or
     Unary(Unary),     // ! or - (negate)
-    Variable(Variable),
+    Variable {
+        name: Token,
+    },
     Range(Range), // range
 }
