3e15f221beb16da400a147f07fb208c476540879
etai432 <etaizilberman@gmail.com>
2023-05-31 01:33:06 +0300

    ok

--- src/parser.rs
+++ src/parser.rs
@@ -13,4 +13,23 @@ impl Parser {
     fn factor() -> Expr {}
     fn unary() -> Expr {}
     fn primary() -> Expr {}
+
+    fn match_tokens(&mut self, types: &[TokenType]) -> bool {
+        //checks if the next token is one of the types
+    }
+    fn check(&self, t_type: TokenType) -> bool {
+        //check if the next token is t_type
+    }
+    fn advance(&mut self) -> Token {
+        //advances by 1
+    }
+    fn is_at_end(&self) -> bool {
+        self.peek().t_type == TokenType::Eof
+    }
+    fn peek(&self) -> Token {
+        self.tokens[self.current].clone()
+    }
+    fn previous(&self) -> Token {
+        self.tokens[self.current - 1].clone()
+    }
 }
