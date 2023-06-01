8fa2ba88efffbb478f29501f87177704d2af6217
decqyd <68643853+decqyd@users.noreply.github.com>
2023-06-01 20:54:57 +0100

    dsd

--- src/stmt.rs
+++ src/stmt.rs
@@ -1,11 +1,30 @@
-use crate::{expr::*, scanner::Value};
+use crate::{
+    expr::*,
+    scanner::{Token, TokenType, Value},
+};
 pub enum Stmt {
-    Print { value: Value },
-    Block,
-    Expression,
-    If,
-    Var,
-    While,
+    Print {
+        value: Value,
+    },
+    Block {
+        statements: Vec<Stmt>,
+    },
+    Expression {
+        expression: Expr,
+    },
+    If {
+        condition: Expr,
+        block: Box<Stmt>,
+        elseblock: Option<Box<Stmt>>,
+    },
+    Var {
+        name: TokenType,
+        value: Option<Expr>,
+    },
+    While {
+        condition: Expr,
+        block: Box<Stmt>,
+    },
     For,
     Fn,
     Return,
